b89321b943afc5cb8a2ee253437bed44a8748812
Pradip Maity <pradipmaity9907@gmail.com>
2026-06-24 18:44:18 +0530

    adding Lec-02

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-console.log("Hello, World!");
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({
+  apiKey: "AIzaSyAJr1nTRf37LLFruLaUIA3qT5z9xsXUWeU",
+});
+
+async function main() {
+  const interaction = await ai.interactions.create({
+    model: "gemini-2.5-flash",
+    input: [
+      {
+        role: "user",
+        parts: [{ text: "what is my name?" }],
+      },
+      {
+        role: "model",
+        parts: [{ text: "I don't know your name. Can you tell me?" }],
+      },
+      {
+        role: "user",
+        parts: [{ text: "My name is Guddu." }],
+      },
+      {
+        role: "model",
+        parts: [{ text: "Nice to meet you, Guddu!" }],
+      },
+      {
+        role: "user",
+        parts: [{ text: "What is my name?" }],
+      },
+    ],
+  });
+  console.log(interaction.output_text);
+}
+
+main();
+
+
+//readline sync
+//history auto save
